78f3bb85c9e145891b94d630846515614c54ffc7
Dane Cronin <danecronin@gmail.com>
2022-11-28 16:40:50 -0700

    added graphql queries for User

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,10 +5,32 @@ const { User, Thought } = require('../models');
 
 const resolvers = {
     Query: {
+        //Get all thoughts
         thoughts: async (parent, {username}) => {
             const params = username ? {username} : {};
             return Thought.find(params).sort({createdAt: -1});
-        }
+        },
+//Get Thought by ID
+        thought: async (parent, {_id}) => {
+            return Thought.findOne({_id});
+        },
+//Get All users
+        users: async () => {
+            return User.find()
+            //omit Mongoose specific __v property and the password
+            .select('-__v -password')
+            //populate withe friends and thoughts data to get any associated data in return
+            .populate('friends')
+            .populate('thoughts');
+        },
+        //Get a user by username
+        user: async (parent, {username}) => {
+            return User.findOne({username})
+            .select('-__v -password')
+            .populate('friends')
+            .populate('thoughts');
+        },
+
     }
 };
 
